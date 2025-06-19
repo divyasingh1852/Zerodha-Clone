56ccbbdcf5aa5ce538c805dc8cd8baf953e1298e
divyasingh1852 <www.punamsingh950@gmail.com>
2025-06-19 22:12:38 +0530

    Allow Render preview host in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,14 +7,36 @@
 // })
 
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//   },
+// });
+
+
+
+
+
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
   },
+  preview: {
+    host: true,
+    port: 8080,
+    allowedHosts: ['zerodha-frontend-0dxv.onrender.com']
+  }
 });
