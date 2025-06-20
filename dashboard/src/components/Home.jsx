11a06745c0690b30c57bb3501461e53f44db377e
divyasingh1852 <www.punamsingh950@gmail.com>
2025-06-20 19:52:02 +0530

    Refactor: Simplify Home.jsx to load TopBar and Dashboard

--- dashboard/src/components/Home.jsx
+++ dashboard/src/components/Home.jsx
@@ -8,34 +8,10 @@ const Home = () => {
   const query = new URLSearchParams(window.location.search);
   const userEmail = query.get("user");
   const navigate = useNavigate();
-
-  const [authChecked, setAuthChecked] = useState(false);      
-  const [isAuthenticated, setIsAuthenticated] = useState(false); 
-  useEffect(() => {
-  const API = process.env.REACT_APP_API_URL || "https://zerodha-backend-89hl.onrender.com";
-
-  axios.post(`${API}/verify`, {}, { withCredentials: true })
-    .then((res) => {
-     if (res.data.status) {
-          setIsAuthenticated(true);                           
-        } else {
-          navigate("/login");
-        }
-    })
-    .catch((err) => {
-      console.error("Verification failed:", err);
-      navigate("/login");
-    })
-    .finally(() => {
-        setAuthChecked(true);                               
-      });
-}, [navigate]);
-
- if (!authChecked) return <p>Checking login...</p>; 
   return (
     <>
       <TopBar />
-      {isAuthenticated && <Dashboard userEmail={userEmail} />} 
+      {<Dashboard userEmail={userEmail} />} 
     </>
   );
 };
